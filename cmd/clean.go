3f9aba8537382ffc19251c1af3b2954896c131b7
Luther Monson <luther.monson@gmail.com>
2021-12-01 10:54:18 -0700

    add extra flags to choose your cleanup

--- cmd/clean.go
+++ cmd/clean.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/goodhosts/hostsfile"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
@@ -14,26 +15,79 @@ func Clean() *cli.Command {
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
 				Name:  "dry-run",
-				Usage: "Dry run only, will output contents of the cleaned hostsfile without writing the changes.",
+				Usage: "Dry run only, will output contents of the cleaned hostsfile without writing the changes",
+			},
+			&cli.BoolFlag{
+				Name:    "all",
+				Usage:   "Perform all Cleanup Jobs",
+				Aliases: []string{"A"},
+			},
+			&cli.BoolFlag{
+				Name:    "remove-duplicate-ips",
+				Usage:   "Remove all duplicate ips",
+				Aliases: []string{"rdi"},
+			},
+			&cli.BoolFlag{
+				Name:    "remove-duplicate-hosts",
+				Usage:   "Remove all duplicate hosts",
+				Aliases: []string{"rdh"},
+			},
+			&cli.BoolFlag{
+				Name:    "sort-hosts",
+				Usage:   "Sort each ip's hosts alphabetically",
+				Aliases: []string{"sh"},
+			},
+			&cli.BoolFlag{
+				Name:    "sort-ips",
+				Usage:   "Sort all ips numerically",
+				Aliases: []string{"si"},
+			},
+			&cli.IntFlag{
+				Name:    "hosts-per-line",
+				Usage:   "Number of hosts allowed per line",
+				Value:   hostsfile.HostsPerLine,
+				Aliases: []string{"hpl"},
 			},
 		},
 	}
 }
 
 func clean(c *cli.Context) error {
-	hostsfile, err := loadHostsfile(c, false)
+	h, err := loadHostsfile(c, false)
 	if err != nil {
 		return err
 	}
 
-	hostsfile.Clean()
+	if c.Int("hosts-per-line") != hostsfile.HostsPerLine {
+		hostsfile.HostsPerLine = c.Int("hosts-per-line")
+	}
+
+	if c.Bool("all") {
+		h.Clean()
+	} else {
+		if c.Bool("remove-duplicate-ips") {
+			h.RemoveDuplicateIps()
+		}
+		if c.Bool("remove-duplicate-hosts") {
+			h.RemoveDuplicateHosts()
+		}
+		if c.Bool("sort-hosts") {
+			h.SortHosts()
+		}
+		if c.Bool("sort-ips") {
+			h.SortByIp()
+		}
+		// needed for windows for 9/line, -1 default for linux will noop but if passed by cli we will run
+		h.HostsPerLine(hostsfile.HostsPerLine)
+	}
+
 	if c.Bool("dry-run") {
 		logrus.Debugln("performing a dry run, writing output")
-		outputHostsfile(hostsfile, true)
+		outputHostsfile(h, true)
 		return debugFooter(c)
 	}
 
-	if err := hostsfile.Flush(); err != nil {
+	if err := h.Flush(); err != nil {
 		return cli.NewExitError(err.Error(), 2)
 	}
 	return debugFooter(c)
